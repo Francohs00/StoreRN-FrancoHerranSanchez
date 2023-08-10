61bcded09a2d51e74cc60ba7af9ff3b4eb923151
Franco Herran Sanchez <francohs00@gmail.com>
2023-08-10 01:01:49 -0300

    09.08

--- src/navigations/tabs.jsx
+++ src/navigations/tabs.jsx
@@ -1,29 +1,73 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons/';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSelector } from 'react-redux';
 
-import { Cart } from '../screens';
+import CartNavigator from './cart';
+import OrdersNavigator from './orders';
+import ShopNavigator from './shop';
 import { COLORS, FONTS } from '../themes';
 
-const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
-const CartNavigator = () => {
+const TabsNavigator = () => {
+  const cartItems = useSelector((state) => state.cart.items);
   return (
-    <Stack.Navigator
-      initialRouteName="Cart"
-      screenOptions={() => ({
-        headerStyle: {
-          backgroundColor: COLORS.primary,
-          height: 80,
+    <BottomTab.Navigator
+      initialRouteName="ShopTab"
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: FONTS.regular,
+          fontSize: 10,
         },
-        headerTitleStyle: {
-          fontFamily: FONTS.bold,
-          fontSize: 16,
+        tabBarStyle: {
+          backgroundColor: COLORS.white,
         },
-        headerTintColor: COLORS.white,
-        animation: 'fade_from_bottom',
-      })}>
-      <Stack.Screen name="Cart" component={Cart} />
-    </Stack.Navigator>
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.gray,
+        tabBarIconStyle: {
+          fontSize: 22,
+        },
+      }}>
+      <BottomTab.Screen
+        name="ShopTab"
+        component={ShopNavigator}
+        options={{
+          tabBarLabel: 'Shop',
+          tabBarIcon: ({ focused, color }) => {
+            return <Ionicons name={focused ? 'home' : 'home-outline'} size={20} color={color} />;
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="CartTab"
+        component={CartNavigator}
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons name={focused ? 'cart' : 'cart-outline'} size={20} color={color} />
+          ),
+          tabBarBadge: cartItems.length,
+          tabBarBadgeStyle: {
+            backgroundColor: COLORS.secodary,
+            color: COLORS.white,
+            fontFamily: FONTS.regular,
+            fontSize: 11,
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="OrdersTab"
+        component={OrdersNavigator}
+        options={{
+          tabBarLabel: 'Orders',
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons name={focused ? 'file-tray' : 'file-tray-outline'} size={20} color={color} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
   );
 };
 
-export default CartNavigator;
+export default TabsNavigator;
